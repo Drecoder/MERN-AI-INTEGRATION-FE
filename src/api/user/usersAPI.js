6e355778254487fbec45a58eff2926ec792f51c8
Andres Arias <ariasa83@gmail.com>
2024-02-15 12:30:51 -0600

    login

--- src/api/user/usersAPI.js
+++ src/api/user/usersAPI.js
@@ -15,3 +15,16 @@ export const registrationAPI = async (userData) => {
  );
     return response?.data;
 };
+export const loginAPI = async (userData) => {
+    const response = await axios.post(
+       'http://localhost:5000/api/v1/users/login',
+      {
+          email: userData?.email,
+          password: userData?.password
+      },
+       {
+           withCredentials: true,
+       }
+    );
+       return response?.data;
+   };
